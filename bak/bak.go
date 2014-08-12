91bf1a3a22cbc8753ec0fea606d3188cb6da11b0
Brian Picciano <mediocregopher@gmail.com>
2014-08-11 21:23:34 -0400

    kinda getting the core logic in there

---
+++ bak/bak.go
@@ -0,0 +1,121 @@
+package bak
+
+import (
+	"github.com/fzzy/radix/extra/sentinel"
+	"github.com/fzzy/radix/redis"
+	//"github.com/fzzy/radix/redis/resp"
+	"io"
+	"log"
+	"strings"
+)
+
+var (
+	sentinelConn   *redis.Client
+	sentinelClient *sentinel.Client
+)
+
+// <CONFIG STUFF>
+var initialBuckets = []string{
+	"bucket0",
+}
+const (
+	SENTINEL_ADDR = "localhost:26379"
+	LOCATOR_NAME = "locator"
+	LOCATOR_SET = "members"
+	LOCATOR_PREFIX = "loc:"
+)
+// </CONFIG STUFF>
+
+func init() {
+	var err error
+	sentinelConn, err = redis.Dial("tcp", SENTINEL_ADDR)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sentinelClient, err = sentinel.NewClient(
+		"tcp",
+		SENTINEL_ADDR,
+		10,
+		initialBuckets...
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bis := make([]interface{}, 0, len(initialBuckets)+1)
+	bis = append(bis, LOCATOR_SET)
+	for i := range bis {
+		bis = append(bis, initialBuckets[i])
+	}
+	_, err = sentinelConn.Cmd("SADD", bis...).Int()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetBucket(bucket string) (*redis.Client, error) {
+	return sentinelClient.GetMaster(bucket)
+}
+
+func PutBucket(bucket string, conn *redis.Client) {
+	sentinelClient.PutMaster(bucket, conn)
+}
+
+// TODO locator stuff should be broken out into its own package
+
+func BucketForKey(key string) (string, error) {
+	if key[len(key)-1] == '}' {
+		if i := strings.Index(key, "{"); i > -1 {
+			key = key[i+1:len(key)-1]
+		}
+	}
+	key = LOCATOR_PREFIX + key
+
+	return bucketForKeyRaw(key)
+}
+
+func tryReturnConn(bucket string, conn *redis.Client, err *error) {
+	if *err == io.EOF {
+		return
+	}
+	PutBucket(bucket, conn)
+}
+
+func bucketForKeyRaw(key string) (string, error) {
+	var conn *redis.Client
+	var err error
+	var bucket string
+	conn, err = GetBucket(LOCATOR_NAME)
+	if err != nil {
+		return "", err
+	}
+	defer tryReturnConn(LOCATOR_NAME, conn, &err)
+
+	r := conn.Cmd("GET", key)
+	if r.Type == redis.ErrorReply {
+		return "", r.Err
+	} else if r.Type == redis.BulkReply  {
+		return r.Str()
+	}
+
+	// we only get here if the key doesn't have a bucket assigned to it yet
+	bucket, err = conn.Cmd("SRANDMEMBER", LOCATOR_SET).Str()
+	if err != nil {
+		return "", err
+	}
+
+	var wasSet int
+	wasSet, err = conn.Cmd("SETNX", key, bucket).Int()
+	if err != nil {
+		return "", err
+	} else if wasSet == 0 {
+		// Another process set the key before we could. We go back to square one
+		// in order to fetch it for real this time
+		return bucketForKeyRaw(key)
+	}
+
+	return bucket, nil
+}
+
+
--- srv/srv.go
+++ srv/srv.go
@@ -2,17 +2,21 @@ package srv
 
 import (
 	"errors"
+	"github.com/fzzy/radix/redis"
 	"github.com/fzzy/radix/redis/resp"
 	"io"
 	"log"
 	"net"
 	"time"
+	
+	"github.com/mediocregopher/breadis/bak"
 )
 
 // errors
 var (
-	errUnkCmd = errors.New("ERR unknown command")
-	errBadCmd = errors.New("ERR bad command")
+	errUnkCmd  = errors.New("ERR unknown command")
+	errBadCmd  = errors.New("ERR bad command")
+	errBackend = errors.New("ERR backend error")
 )
 
 
@@ -36,12 +40,12 @@ func handleConnection(conn net.Conn) {
 	log.Println("got connection")
 
 	var m *resp.Message
-	var ms []*resp.Message
 	var err error
 	for {
 		err = conn.SetReadDeadline(time.Now().Add(1 * time.Second))
 		if err != nil {
 			log.Println("error setting read deadline")
+			conn.Close()
 			return
 		}
 
@@ -58,16 +62,67 @@ func handleConnection(conn net.Conn) {
 			continue
 		}
 
-		ms, err = m.Array()
-		if err != nil || len(ms) < 2 || ms[1].Type != resp.BulkStr {
+		handleCommand(conn, m)
+	}
+
+}
+
+func handleCommand(conn net.Conn, m *resp.Message) {
+	var err error
+	var ms []*resp.Message
+	var key, bucket string
+	var cmd []interface{}
+	var rconn *redis.Client
+	ms, err = m.Array()
+	if err != nil || len(ms) < 2 || ms[1].Type != resp.BulkStr {
+		resp.WriteArbitrary(conn, errBadCmd)
+		return
+	}
+
+	for i := range ms {
+		cmd[i], err = ms[i].Str()
+		if err != nil {
 			resp.WriteArbitrary(conn, errBadCmd)
-			return 
+			return
 		}
+	}
 
-		cmd, _ := ms[0].Str()
-		key, _ := ms[1].Str()
-		log.Println(cmd, key)
+	key = cmd[1].(string)
 
-		resp.WriteMessage(conn, m)
+	bucket, err = bak.BucketForKey(key)
+	if err != nil {
+		log.Printf("BucketForKey(%s): %s", key, err)
+		resp.WriteArbitrary(conn, errBackend)
+		return
+	}
+
+	rconn, err = bak.GetBucket(bucket)
+	if err != nil {
+		log.Printf("GetBucket(%s): %s", bucket, err)
+		resp.WriteArbitrary(conn, errBackend)
+		return
+	}
+
+	
+}
+
+func forwardCommand(
+	// TODO Pass in the message itself instead of cmd. We would need to allow
+	// for getting the raw connection out of the redis client. Shouldn't be too
+	// difficult. DO THIS NOW YOU MUST DO IT THERE IS NO CHOICE
+	conn net.Conn, rconn *redis.Client, cmd []interface{},
+) (
+	*resp.Message, bool,
+) {
+	var err error
+	var r *redis.Reply
+
+	r = rconn.Cmd(cmd[0].(string), cmd[1:]...)
+	if r.Type == redis.ErrorReply {
+		if r.Err.Error()[:3] == "ERR" {
+			return r, true
+		} else {
+			r.Err = errBackend
+		}
 	}
 }
